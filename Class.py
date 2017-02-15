b32de57190d8c5d25f4928e4e2081b3b49ed59b2
ebrahim-j <ebzjanoo@gmail.com>
2017-02-15 15:01:58 +0300

    Update Class.py
    change class name to "Klass" to avoid inbuilt type collisions
    Import required libararies
    Print  statement in list_classes def corrected

--- Class.py
+++ Class.py
@@ -1,4 +1,11 @@
-class Class():
+from random import randint
+import time
+from Student import Student
+
+
+class Klass():
+
+    class_id = [] 
 
     def __init__(self, id, name, start, stop, status ):
         self.id = id
@@ -13,42 +20,46 @@ class Class():
         start = None
         stop = None
         status = "Not in session"
-        id = randint(100,999)
-        while id in class_id or id == 0:
+        id = randint(100,999) 
+        while id in Klass.class_id or id == 0:
             id = randint(100,999)
-        class_id.append(id)
-        new_class = Class(id, name, start, stop, status)
-        classes[id] = new_class
+        Klass.class_id.append(id)
+        new_class = Klass(id, name, start, stop, status)
+        Klass.classes[id] = new_class
         print("Done Successfully!\nYour class ID is: ", id)
             
             
     def remove_class():
         ask = int(input("Which class do you want to remove? "))
-        if ask in classes:
-            del classes[ask]
+        if ask in Klass.classes:
+            del Klass.classes[ask]
             print("Deleted!")
         else:
             print("This class was not in our system anyway!")
 
     def log_in():
-        ask = int(input("Enter class ID to log in: ")) #what if already logged in???
-        localtime = time.localtime()
-        logTime = time.strftime("%d-%b-%Y %r", localtime)
-        classes[ask].start = logTime
-        classes[ask].status = "Class in session"
-        print("Logged in Successfully")
+        ask = int(input("Enter class ID to log in: ")) 
+        if  Klass.classes[ask].status == "Class in session": #what if already logged in???
+            print("Class already logged in!")
+        else:
+            localtime = time.localtime()
+            logTime = time.strftime("%d-%b-%Y %H:%M:%S", localtime)
+            Klass.classes[ask].start = logTime
+            Klass.classes[ask].status = "Class in session"
+            print("Logged in Successfully at :", logTime)
             
                             
     def log_out(): 
         ask = int(input("Enter class Id to be logged out: "))
         localtime = time.localtime()
-        logTime = time.strftime("%d-%b-%Y %r", localtime)
-        classes[ask].stop = logTime
-        classes[ask].status = "Not in session"
-        print("Logged out Successfully")
+        logTime = time.strftime("%d-%b-%Y %H:%M:%S", localtime)
+        Klass.classes[ask].stop = logTime
+        Klass.classes[ask].status = "Not in session"
+
+        print("Logged out  at: ", logTime)
 
     def list_classes():
-        for key in classes:
+        for key in Klass.classes:
             tally = Student.students_in_class(key)
-            print ("{} - {}", classes[key].name, tally)
+            print ("{} - {}", Klass.classes[key].name, tally)
 
