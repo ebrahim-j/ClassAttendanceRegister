7bb4cb0fc94b2c315408241d1a7246b3c7924e8f
ebrahim-j <ebzjanoo@gmail.com>
2017-02-16 14:48:50 +0300

    Update Reason.py
    Import required module functions.
    Correct calling method

--- Reason.py
+++ Reason.py
@@ -1,4 +1,10 @@
+from Student import Student
+from Klass import Klass
+
 class Reason():
+
+    reasons = {}
+
     def __init__(self, student_id, class_id, reason):
         self.student_id = student_id
         self.class_id = class_id
@@ -10,12 +16,12 @@ class Reason():
         st_id = int(input("Which student(By ID please)?" ))	
         reason = input("Kindly state reason: ")
         
-        if st_id not in students:
+        if st_id not in Student.students:
             print("Who is this dude? We don't know him!")
-        elif cl_id not in classes:
+        elif cl_id not in Klass.classes:
             print("Ey bana! This class does not exist!")
         else:
             new_reason = Reason(st_id, cl_id, reason) #try to add a when eg by datetime
-            reasons[reason] = new_reason
-            students[st_id].status = "Not in class"
-            print("{} is allowed to leave!".format(students[st_id].name)) 
+            Reason.reasons[reason] = new_reason
+            Student.students[st_id].status = "Not in class"
+            print("{} is allowed to leave!".format(Student.students[st_id].name)) 
